255ba96fc050cead2df7e4ff31710ca911ffd7e9
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2019-02-01 08:13:45 +0100

    Check compatibility for Moodle 3.6, no functionality change.

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_opcache';
 $plugin->version = 2018052400;
 $plugin->release = 'v3.5-r1';
-$plugin->requires = 2018051700;
+$plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
