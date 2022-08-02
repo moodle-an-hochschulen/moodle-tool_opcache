474978d35e0f546c81305c06df4d5cba525941bc
Alexander Bias <bias@alexanderbias.de>
2022-08-02 21:39:06 +0200

    Release v3.11-r3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opcache';
-$plugin->version = 2021051701;
-$plugin->release = 'v3.11-r2';
+$plugin->version = 2021051702;
+$plugin->release = 'v3.11-r3';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
