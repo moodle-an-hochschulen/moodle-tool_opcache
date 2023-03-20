1433de04a360ca438f943130295dd0138ab1eb05
Alexander Bias <bias@alexanderbias.de>
2023-03-20 21:21:02 +0100

    Prepare compatibility for Moodle 4.1.

--- CHANGES.md
+++ CHANGES.md
@@ -6,6 +6,7 @@ Changes
 
 ### Unreleased
 
+* 2023-01-21 - Prepare compatibility for Moodle 4.1.
 * 2023-03-11 - Make codechecker happy again
 * 2022-11-28 - Updated Moodle Plugin CI to latest upstream recommendations
 
--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_opcache';
 $plugin->version = 2022071201;
 $plugin->release = 'v4.0-r2';
-$plugin->requires = 2022041900;
-$plugin->supported = [400, 400];
+$plugin->requires = 2022112800;
+$plugin->supported = [401, 401];
 $plugin->maturity = MATURITY_STABLE;
