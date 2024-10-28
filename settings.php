505791c9cbe0156406b12e692c00ab4189ae6633
Alexander Bias <abias@ssystems.de>
2024-10-28 15:51:14 +0100

    Upgrade: Adopt changes from MDL-81920 and use new \core\lang_string class.

--- settings.php
+++ settings.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
     // Add the plugin's main page to the server settings category.
-    $settingspage = new admin_externalpage('tool_opcache', new lang_string('pluginname', 'tool_opcache'),
+    $settingspage = new admin_externalpage('tool_opcache', new \core\lang_string('pluginname', 'tool_opcache'),
                                            new moodle_url('/'.$CFG->admin.'/tool/opcache/index.php'), 'moodle/site:config');
     $ADMIN->add('server', $settingspage);
 }
