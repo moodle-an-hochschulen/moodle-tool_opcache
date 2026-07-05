89466cf3425af274091c741a11e068294f54e1a0
Alexander Bias <bias@alexanderbias.de>
2026-07-05 10:48:51 +0200

    Release v5.0-r5

--- CHANGES.md
+++ CHANGES.md
@@ -4,7 +4,7 @@ moodle-tool_opcache
 Changes
 -------
 
-### Unreleased
+### v5.0-r5
 
 * 2026-07-05 - Upgrade OPcache GUI to version 3.6.1
 
--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opcache';
-$plugin->version = 2025041403;
-$plugin->release = 'v5.0-r4';
+$plugin->version = 2025041404;
+$plugin->release = 'v5.0-r5';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 500];
 $plugin->maturity = MATURITY_STABLE;
